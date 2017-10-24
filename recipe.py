9d1e860cfb74880670e5a69152282aa46acadd61
dbdennot8 <dennisbm91@gmailcom>
2017-10-24 12:32:34 +0300

    Seperate functionality and tests, Add functionality

--- recipe.py
+++ recipe.py
@@ -1,6 +1,5 @@
-'''This issa Roman Numeral Converter'''
+'''This issa Roman Numeral Converter that also does the reverse'''
 
-import unittest
 
 class RomanNumeralConverter(object):
     def __init__(self):
@@ -20,60 +19,31 @@ class RomanNumeralConverter(object):
         else:
             raise TypeError("Input type Unrecognized")
 
-class RomanNumeralConverterTest(unittest.TestCase):
-    
-    def setUp(self):
-        # print("Creating a new instance of RomanNumeralConverter class for testing")
-        self.value = RomanNumeralConverter()
-    
-    def tearDown(self):
-        # print("Destroying nolonger needed instance of RomanNumeralConverter class once testing is done\n")
-        self.value = None
+    def convert_to_roman(self, decimal_number):
+        '''Returns a roman numeral given a decimal number'''
+        self.decimal_number = decimal_number
 
-    def test_parsing_millenia(self):
-        '''Does it return 1000 when parsed "M"?'''
-        self.assertEqual(self.value.convert_to_decimal("M"), 1000)
+        val = ""
+        while decimal_number > 1000:
+            val += "M"
+            decimal_number -= 1000
+        while decimal_number > 500:
+            val += "D"
+            decimal_number -= 500
+        while decimal_number > 100:
+            val += "C"
+            decimal_number -= 100
+        while decimal_number > 50:
+            val += "L"
+            decimal_number -= 50
+        while decimal_number > 10:
+            val += "X"
+            decimal_number -= 10
+        while decimal_number > 5:
+            val += "V"
+            decimal_number -= 5
+        while decimal_number >= 1:
+            val += "I"
+            decimal_number -= 1
+        return val
 
-    def test_parsing_half_millenia(self):
-        '''Does it return 500 when parsed "D"?'''
-        self.assertEqual(self.value.convert_to_decimal("D"), 500)
-
-    def test_parsing_century(self):
-        '''Does it return 100 when parsed "C"?'''
-        self.assertEqual(self.value.convert_to_decimal("C"), 100)
-
-    def test_parsing_half_century(self):
-        '''Does it return 50 when parsed "L"?'''
-        self.assertEqual(self.value.convert_to_decimal("L"), 50) 
-
-    def test_parsing_decade(self):
-        '''Does it return 10 when parsed "X"?'''
-        self.assertEqual(self.value.convert_to_decimal("X"), 10)   
-
-    def test_parsing_half_decade(self):
-        '''Does it return 5 when parsed "V"?'''
-        self.assertEqual(self.value.convert_to_decimal("V"), 5)   
-
-    def test_parsing_one(self):
-        '''Does it return 1 when parsed "I"?'''
-        self.assertEqual(self.value.convert_to_decimal("I"), 1)  
-
-    def test_empty_roman_numeral(self):
-        '''Does it return 0 when parsed an empty string?'''
-        self.assertTrue(self.value.convert_to_decimal(" ") == 0)
-        self.assertFalse(self.value.convert_to_decimal(" ") > 0)
-    
-    def test_no_roman_numeral(self):
-        '''Does it raise a TypeError when parsed nothing?'''
-        self.assertRaises(TypeError, self.value.convert_to_decimal, None)
-        
-if __name__ == '__main__':
-    import sys
-    suite = unittest.TestSuite()
-    if len(sys.argv) == 1:
-        suite = unittest.TestLoader().loadTestsFromTestCase(RomanNumeralConverterTest)
-    else:
-        for test_name in sys.argv[1:]:
-            suite.addTest(RomanNumeralConverterTest(test_name))
-
-    unittest.TextTestRunner(verbosity=2).run(suite)
